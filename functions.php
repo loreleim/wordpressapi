2a5fbe88ddcca1f57dd9efae57fd14c666760a88
Lorelei Miyamura <lorelei.miyamura@gmail.com>
2021-01-29 17:32:48 -0800

    fixing naming for specific project

--- functions.php
+++ functions.php
@@ -4,19 +4,18 @@
 define( 'GENERATE_VERSION', '0.1.0' );
 
 //Enqueue scripts and styles.
-function khro_scripts() {
-	wp_enqueue_style( 'khro-style', get_template_directory_uri() . "/style.css", array(), _S_VERSION );
+function wordpressapi_scripts() {
+  wp_enqueue_style( 'wordpressapi-style', get_template_directory_uri() . "/style.css", array(), _S_VERSION );
 }
-add_action( 'wp_enqueue_scripts', 'khro_scripts' );
+add_action( 'wp_enqueue_scripts', 'wordpressapi_scripts' );
 
 
-if ( ! function_exists( 'khro_setup' ) ) :
-
-	function khro_setup() {
-	// adds theme support for various features
-	add_theme_support( 'title-tag' ); //change <title>
+  function wordpressapi_setup() {
+  // adds theme support for various features
+  add_theme_support( 'title-tag' ); //change <title>
     add_theme_support( 'post-thumbnails' );
 
-	}
-endif;
-add_action( 'after_setup_theme', 'khro_setup' );
+  }
+add_action( 'after_setup_theme', 'wordpressapi_setup' );
+
+
